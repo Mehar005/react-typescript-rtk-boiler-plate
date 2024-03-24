bc3579a12bdf1a28e43d623803535de5e6f7d1b7
Asim Ali <asimali35.aa@gmail.com>
2024-03-25 03:16:45 +0500

    Boiler plate with RTK

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@src': path.resolve(__dirname, 'src'),
+			'@store': path.resolve(__dirname, 'src/store'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@hooks': path.resolve(__dirname, 'src/hooks'),
+			'@modules': path.resolve(__dirname, 'src/modules'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@utilities': path.resolve(__dirname, 'src/utilities'),
+			'@theme': path.resolve(__dirname, 'src/theme'),
+			'@constants': path.resolve(__dirname, 'src/constants'),
+			'@HOCs': path.resolve(__dirname, 'src/HOCs'),
+			'@types': path.resolve(__dirname, 'src/types'),
+		},
+	},
+});
